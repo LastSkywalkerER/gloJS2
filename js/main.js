089b38d000471ae505c99ffbed9eb804e491c19b
LastSkywalkerER <maxdr1998@gmail.com>
2021-02-26 16:54:56 +0300

    add tabs

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ window.addEventListener('DOMContentLoaded', () => {
     updateClock();
   }
 
-  countTimer('2021-02-26');
+  countTimer('2021-03-01');
 
   // menu
 
@@ -116,5 +116,42 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   togglePopUp();
+
+  //tubs
+
+  const tabs = () => {
+    const tabHeader = document.querySelector('.service-header'),
+      tab = tabHeader.querySelectorAll('.service-header-tab'),
+      tabContent = document.querySelectorAll('.service-tab');
+
+    const toggleTabContent = (index) => {
+
+      for (let i = 0; i < tabContent.length; i++) {
+        if (index === i) {
+          tab[i].classList.add('active');
+          tabContent[i].classList.remove('d-none');
+        } else {
+          tab[i].classList.remove('active');
+          tabContent[i].classList.add('d-none');
+        }
+      }
+    }
+
+    tabHeader.addEventListener('click', (event) => {
+      let target = event.target;
+      target = target.closest('.service-header-tab');
+
+      if (target) {
+        tab.forEach((item, i) => {
+          if (item === target) {
+            toggleTabContent(i);
+          }
+        })
+      }
+    })
+
+  }
+
+  tabs();
 
 });
