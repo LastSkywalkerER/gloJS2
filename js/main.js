0ce66a86cb2aa8374233afff44c449aa21e94e65
LastSkywalkerER <maxdr1998@gmail.com>
2021-03-01 17:59:07 +0300

    step1

--- js/main.js
+++ js/main.js
@@ -277,4 +277,21 @@ window.addEventListener('DOMContentLoaded', () => {
 
   slider();
 
+  const changeImage = () => {
+    const imgs = document.querySelectorAll('.command__photo');
+
+    const toggleImage = event => {
+      const src = event.target.src;
+      event.target.src = event.target.dataset.img;
+      event.target.dataset.img = src;
+    };
+
+    imgs.forEach(item => {
+      item.addEventListener('mouseover', toggleImage);
+      item.addEventListener('mouseout', toggleImage);
+    });
+  };
+
+  changeImage();
+
 });
