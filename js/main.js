54179d46169e1b78a98fb1c022bb9f55936ca083
LastSkywalkerER <maxdr1998@gmail.com>
2021-03-02 17:37:17 +0300

    on video

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ window.addEventListener('DOMContentLoaded', () => {
     updateClock();
   }
 
-  countTimer('2021-03-01');
+  countTimer('2021-03-22');
 
   // menu
   const toggleMenu = () => {
@@ -351,5 +351,45 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   validation();
+
+  // Калькулятор
+
+  const calc = (price = 100) => {
+
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcCount = document.querySelector('.calc-count'),
+      calcDay = document.querySelector('.calc-day'),
+      totalValue = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+      const typeValue = calcType.value,
+        squareValue = calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+
+      totalValue.textContent = Math.floor(total);
+    };
+
+    calcBlock.addEventListener('input', countSum);
+  };
+
+  calc();
 
 });
